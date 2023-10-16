87b5b0a48395709c223b0884020eac31eb4322b9
Domenica Escobar <domenica.escobar.moyano@udla.edu.ec>
2023-10-16 07:33:44 -0600

    Nueva clase Menu

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DE_WebCambios.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DE_WebCambios.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public bool ConSalsa { get; set; }
+        [Range(0.01, 9999.99)]
+        public decimal Precio { get; set; }
+    }
+}
